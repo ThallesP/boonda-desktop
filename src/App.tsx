423b468d5eb8d3f2a2b1a427785bc952423d199f
Thalles Passos <contato@thalles.me>
2024-04-18 17:36:43 -0300

    fix: button is taking the entire width

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { appWindow } from "@tauri-apps/api/window";
 function App() {
   return (
     <TanStackQueryProvider>
-      <button
-        className="bg-zinc-950 text-zinc-700 w-full absolute flex flex-row-reverse"
-        onClick={() => appWindow.hide()}
-      >
-        <X size={24} />
-      </button>
+      <div className="bg-zinc-950 flex absolute justify-end items-center w-full">
+        <button
+          className="text-zinc-700 flex flex-row-reverse"
+          onClick={() => appWindow.hide()}
+        >
+          <X size={24} />
+        </button>
+      </div>
       <div className="bg-zinc-950 h-screen w-full flex justify-center items-center">
         <div className="flex flex-col gap-16 w-full">
           <UploadComponent />
