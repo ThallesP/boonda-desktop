a24335f676e2a7921120fc3fa2ba23308e16a3ae
Thalles Passos <contato@thalles.me>
2024-04-21 20:13:01 -0300

    feat: allow to specify custom token

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,4 +1,4 @@
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from "@/lib/constants";
 import { createClient as createSupabaseClient } from "@supabase/supabase-js";
-export const createClient = () =>
-  createSupabaseClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const createClient = (customToken?: string) =>
+  createSupabaseClient(SUPABASE_URL, customToken ?? SUPABASE_ANON_KEY);
